bad1aba403c233ea74f12346326bb4774d6decb6
laridevops <nkanalarichou@gmail.com>
2024-01-09 21:30:53 -0600

    new server

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "server1" {
     Name = "devops-server"
   }
 }
+
+resource "aws_instance" "server2" {
+  instance_type = var.instance_type
+  ami = data.aws_ami.ami1.id
+
+  tags = {
+    Name = "dev-server"
+  }
+}
